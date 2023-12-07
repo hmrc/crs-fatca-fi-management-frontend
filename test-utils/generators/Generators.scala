550642e6f6fc5da12bd763d4b21086aea2416e98
Chris Waugh <238270+chriswaugh@users.noreply.github.com>
2023-12-07 13:29:11 +0000

    Revert "Revert "DAC6-2733 | Added regex generator dependency and contact name page""

--- test-utils/generators/Generators.scala
+++ test-utils/generators/Generators.scala
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package generators
 
 import java.time.{Instant, LocalDate, ZoneOffset}
-
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Gen._
 import org.scalacheck.{Gen, Shrink}
+import wolfendale.scalacheck.regexp.RegexpGen
 
 trait Generators extends ModelGenerators {
 
@@ -25,10 +41,24 @@ trait Generators extends ModelGenerators {
     }
   }
 
+  def stringMatchingRegexAndLength(regex: String, length: Int): Gen[String] =
+    RegexpGen
+      .from(regex)
+      .suchThat(
+        value => value.trim.nonEmpty
+      )
+      .map(_.take(length))
+
   def intsInRangeWithCommas(min: Int, max: Int): Gen[String] = {
     val numberGen = choose[Int](min, max).map(_.toString)
     genIntersperseString(numberGen, ",")
   }
+
+  def stringsLongerThanAlpha(minLength: Int): Gen[String] = for {
+    maxLength <- (minLength * 2).max(100)
+    length    <- Gen.chooseNum(minLength + 1, maxLength)
+    chars     <- listOfN(length, Gen.alphaChar)
+  } yield chars.mkString
 
   def intsLargerThanMaxValue: Gen[BigInt] =
     arbitrary[BigInt] suchThat (
