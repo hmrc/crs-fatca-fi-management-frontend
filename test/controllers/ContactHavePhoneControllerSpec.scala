986f9c986fec876224c48ffe2dccdf1be0c9420c
AashuChauhanHmrc <aashu.chauhan@digital.hmrc.gov.uk>
2024-01-05 12:33:16 +0000

    added contact have phone page

--- test/controllers/ContactHavePhoneControllerSpec.scala
+++ test/controllers/ContactHavePhoneControllerSpec.scala
@@ -1,13 +1,29 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
 import forms.ContactHavePhoneFormProvider
-import models.{NormalMode, UserAnswers}
+import models.{Name, NormalMode, UserAnswers}
 import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import pages.ContactHavePhonePage
+import pages.{ContactHavePhonePage, ContactNamePage}
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
@@ -22,15 +38,19 @@ class ContactHavePhoneControllerSpec extends SpecBase with MockitoSugar {
   def onwardRoute = Call("GET", "/foo")
 
   val formProvider = new ContactHavePhoneFormProvider()
-  val form = formProvider()
+  val form         = formProvider()
+  val FirstName    = "Fred"
+  val LastName     = "Flintstone"
+  val name: Name   = Name(FirstName, LastName)
 
   lazy val contactHavePhoneRoute = routes.ContactHavePhoneController.onPageLoad(NormalMode).url
+  val userAnswers                = emptyUserAnswers.set(ContactNamePage, name.fullName).success.value
 
   "ContactHavePhone Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
       running(application) {
         val request = FakeRequest(GET, contactHavePhoneRoute)
@@ -40,13 +60,18 @@ class ContactHavePhoneControllerSpec extends SpecBase with MockitoSugar {
         val view = application.injector.instanceOf[ContactHavePhoneView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form, name.fullName, NormalMode)(request, messages(application)).toString
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
-
-      val userAnswers = UserAnswers(userAnswersId).set(ContactHavePhonePage, true).success.value
+      val userAnswers = emptyUserAnswers
+        .set(ContactNamePage, name.fullName)
+        .success
+        .value
+        .set(ContactHavePhonePage, true)
+        .success
+        .value
 
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
@@ -58,7 +83,7 @@ class ContactHavePhoneControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form.fill(true), name.fullName, NormalMode)(request, messages(application)).toString
       }
     }
 
@@ -90,7 +115,7 @@ class ContactHavePhoneControllerSpec extends SpecBase with MockitoSugar {
 
     "must return a Bad Request and errors when invalid data is submitted" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
       running(application) {
         val request =
@@ -104,7 +129,7 @@ class ContactHavePhoneControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, name.fullName, NormalMode)(request, messages(application)).toString
       }
     }
 
@@ -138,4 +163,5 @@ class ContactHavePhoneControllerSpec extends SpecBase with MockitoSugar {
       }
     }
   }
+
 }
