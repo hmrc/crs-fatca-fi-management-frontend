15ac7f6b3500acef7f12cab546a47ee7d7838b6d
matthewharris1 <33547722+matthewharris1@users.noreply.github.com>
2024-01-25 15:11:15 +0000

    DAC6-3024 | updated specs

--- test/controllers/changeContactDetails/IndividualPhoneControllerSpec.scala
+++ test/controllers/changeContactDetails/IndividualPhoneControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.changeContactDetails
 
 import base.SpecBase
@@ -14,6 +30,7 @@ import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import repositories.SessionRepository
+import views.html.changeContactDetails.IndividualPhoneView
 
 import scala.concurrent.Future
 
@@ -24,7 +41,7 @@ class IndividualPhoneControllerSpec extends SpecBase with MockitoSugar {
   val formProvider = new IndividualPhoneFormProvider()
   val form         = formProvider()
 
-  lazy val individualPhoneRoute = routes.IndividualPhoneController.onPageLoad(NormalMode).url
+  lazy val individualPhoneRoute = controllers.changeContactDetails.routes.IndividualPhoneController.onPageLoad(NormalMode).url
 
   "IndividualPhone Controller" - {
 
