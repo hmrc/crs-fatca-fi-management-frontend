0e6880d35f4b49b8510c13e8e5c0a0f211ae7c18
chriswaugh <238270+chriswaugh@users.noreply.github.com>
2024-03-27 17:07:55 +0000

    DAC6-3131: Updated feedback-frontend config

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -29,10 +29,9 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val appName: String = configuration.get[String]("appName")
 
   private val contactHost                  = configuration.get[String]("contact-frontend.host")
-  private val contactFormServiceIdentifier = "crs-fatca-fi-management-frontend"
 
   def feedbackUrl(implicit request: RequestHeader): String =
-    s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
+    s"$contactHost/contact/beta-feedback?service=$appName&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
 
   val loginUrl: String              = configuration.get[String]("urls.login")
   val loginContinueUrl: String      = configuration.get[String]("urls.loginContinue")
@@ -40,8 +39,8 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   lazy val lostUTRUrl: String       = configuration.get[String]("urls.lostUTR")
   lazy val addressLookUpUrl: String = configuration.get[Service]("microservice.services.address-lookup").baseUrl
 
-  private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
-  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/crs-fatca-fi-management-frontend"
+  private val exitSurveyBaseUrl: String = configuration.get[String]("feedback-frontend.host")
+  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/$appName"
 
   val languageTranslationEnabled: Boolean =
     configuration.get[Boolean]("features.welsh-translation")
