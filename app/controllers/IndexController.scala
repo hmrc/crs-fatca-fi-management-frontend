7d90220433ccefb66ef9838ac976b5370a4846e4
matthewharris1 <33547722+matthewharris1@users.noreply.github.com>
2024-05-14 10:43:35 +0100

    DAC6-3039 | Removed unused import

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -18,7 +18,7 @@ package controllers
 
 import config.FrontendAppConfig
 import controllers.actions.IdentifierAction
-import models.{Mode, UserAnswers}
+import models.UserAnswers
 import play.api.Logging
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
