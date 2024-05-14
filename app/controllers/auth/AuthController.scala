3ed1cb73641fddb061ae4b1ca6eb82418649d191
chriswaugh <238270+chriswaugh@users.noreply.github.com>
2024-05-14 10:39:35 +0100

    Reverted AuthController.signOut

--- app/controllers/auth/AuthController.scala
+++ app/controllers/auth/AuthController.scala
@@ -41,7 +41,7 @@ class AuthController @Inject() (
         .clear(request.userId)
         .map {
           _ =>
-            Redirect(config.signOutUrl).withSession()
+            Redirect(config.signOutUrl, Map("continue" -> Seq(config.exitSurveyUrl)))
         }
   }
 
