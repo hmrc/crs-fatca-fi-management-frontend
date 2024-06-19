dbca170aa0bb6e622526e644164c7c8971bb0dc6
Carl Thompson <carl.thompson@opencastsoftware.com>
2024-06-19 03:03:25 +0100

    Scaffold check-details

---
+++ test/controllers/CheckDetailsControllerSpec.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import base.SpecBase
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.addFinancialInstitution.IsRegisteredBusiness.CheckDetailsView
+
+class CheckDetailsControllerSpec extends SpecBase {
+
+  "CheckDetails Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.CheckDetailsController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[CheckDetailsView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view()(request, messages(application)).toString
+      }
+    }
+  }
+
+}
