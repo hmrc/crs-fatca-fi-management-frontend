ad3a8d9a6d28640c8b559ed5ca29bab5c22ddf9d
matthewharris1 <33547722+matthewharris1@users.noreply.github.com>
2024-09-02 11:01:22 +0100

    DAC6-3209 | Scaffolded and amended new page + CYA validator

---
+++ app/controllers/SomeInformationMissingController.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import controllers.actions._
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.SomeInformationMissingView
+
+class SomeInformationMissingController @Inject()(
+                                       override val messagesApi: MessagesApi,
+                                       identify: IdentifierAction,
+                                       getData: DataRetrievalAction,
+                                       requireData: DataRequiredAction,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       view: SomeInformationMissingView
+                                     ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      Ok(view())
+  }
+}
