2db2dedeee50002632d939a977923c186e5c0fc2
matthewharris1 <33547722+matthewharris1@users.noreply.github.com>
2024-09-25 11:19:14 +0100

    DAC6-3047 | Amended spec

--- test/controllers/DetailsUpdatedControllerSpec.scala
+++ test/controllers/DetailsUpdatedControllerSpec.scala
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
+import pages.addFinancialInstitution.{FirstContactNamePage, NameOfFinancialInstitutionPage}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import views.html.DetailsUpdatedView
@@ -11,7 +28,9 @@ class DetailsUpdatedControllerSpec extends SpecBase {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val userAnswers = emptyUserAnswers.set(NameOfFinancialInstitutionPage, "answer").success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
       running(application) {
         val request = FakeRequest(GET, routes.DetailsUpdatedController.onPageLoad().url)
@@ -21,7 +40,7 @@ class DetailsUpdatedControllerSpec extends SpecBase {
         val view = application.injector.instanceOf[DetailsUpdatedView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view()(request, messages(application)).toString
+        contentAsString(result) mustEqual view("answer")(request, messages(application)).toString
       }
     }
   }
