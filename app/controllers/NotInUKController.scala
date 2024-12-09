55ca5a57b8c97f8ec048e1730a53c98cf3a984d2
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2024-12-09 09:56:52 +0000

    DAC6-3415 | amended unused parameters in controller + model

--- app/controllers/NotInUKController.scala
+++ app/controllers/NotInUKController.scala
@@ -26,14 +26,12 @@ import views.html.NotInUKView
 class NotInUKController @Inject() (
   override val messagesApi: MessagesApi,
   identify: IdentifierAction,
-  getData: DataRetrievalAction,
-  requireData: DataRequiredAction,
   val controllerComponents: MessagesControllerComponents,
   view: NotInUKView
 ) extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad: Action[AnyContent] = identify {
     implicit request =>
       Ok(view())
   }
