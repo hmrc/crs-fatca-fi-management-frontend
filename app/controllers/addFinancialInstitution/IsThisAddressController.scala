0168a5e225696dd28f78c29ef8065531934118a9
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2024-12-09 11:11:32 +0000

    DAC6-3415 | Removed unused match

--- app/controllers/addFinancialInstitution/IsThisAddressController.scala
+++ app/controllers/addFinancialInstitution/IsThisAddressController.scala
@@ -74,14 +74,11 @@ class IsThisAddressController @Inject() (
               formWithErrors =>
                 Future.successful(BadRequest(view(formWithErrors, mode, getFinancialInstitutionName(request.userAnswers), address.head.toAddress))),
               value =>
-                (address.head.country, ua.get(IsThisAddressPage)) match {
-                  case _ =>
-                    for {
-                      updatedAnswers                    <- Future.fromTry(ua.set(IsThisAddressPage, value))
-                      updatedAnswersWithSelectedAddress <- Future.fromTry(updatedAnswers.set(SelectedAddressLookupPage, address.head))
-                      _                                 <- sessionRepository.set(updatedAnswersWithSelectedAddress)
-                    } yield Redirect(navigator.nextPage(IsThisAddressPage, mode, updatedAnswersWithSelectedAddress))
-                }
+                for {
+                  updatedAnswers                    <- Future.fromTry(ua.set(IsThisAddressPage, value))
+                  updatedAnswersWithSelectedAddress <- Future.fromTry(updatedAnswers.set(SelectedAddressLookupPage, address.head))
+                  _                                 <- sessionRepository.set(updatedAnswersWithSelectedAddress)
+                } yield Redirect(navigator.nextPage(IsThisAddressPage, mode, updatedAnswersWithSelectedAddress))
             )
         case None => Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
 
