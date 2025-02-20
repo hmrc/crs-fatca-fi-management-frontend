232e1623f4b1f01918011a82f618cb3ad745adb3
Carl Thompson <carl.thompson@opencastsoftware.com>
2025-02-20 17:31:47 +0000

    OtherAccessControllerSpec updated

--- test/controllers/OtherAccessControllerSpec.scala
+++ test/controllers/OtherAccessControllerSpec.scala
@@ -19,10 +19,12 @@ package controllers
 import base.SpecBase
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
 import forms.OtherAccessFormProvider
+import models.UserAnswers
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatest.PrivateMethodTester
 import org.scalatestplus.mockito.MockitoSugar
+import pages.RemoveInstitutionDetail
 import play.api.data.Form
 import play.api.inject.bind
 import play.api.mvc.Call
@@ -44,13 +46,16 @@ class OtherAccessControllerSpec extends SpecBase with MockitoSugar with PrivateM
   val fiIsUser            = true
 
   lazy val otherAccessRoute: String                                  = routes.OtherAccessController.onPageLoad(testFiDetail.FIID).url
+  lazy val otherAccessPostRoute: String                              = routes.OtherAccessController.onSubmit().url
   val mockFinancialInstitutionsService: FinancialInstitutionsService = mock[FinancialInstitutionsService]
+  val mockSessionRepository: SessionRepository                       = mock[SessionRepository]
+  val ua: UserAnswers                                                = emptyUserAnswers.withPage(RemoveInstitutionDetail, testFiDetail)
 
   "OtherAccess Controller" - {
 
+    when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
     when(mockFinancialInstitutionsService.getListOfFinancialInstitutions(any())(any[HeaderCarrier](), any[ExecutionContext]()))
       .thenReturn(Future.successful(testFiDetails))
-
     when(mockFinancialInstitutionsService.getInstitutionById(Seq(any()), any())).thenReturn(Some(testFiDetail))
 
     "must return OK and the correct view for a GET" in {
@@ -67,45 +72,45 @@ class OtherAccessControllerSpec extends SpecBase with MockitoSugar with PrivateM
         val view = application.injector.instanceOf[OtherAccessView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, fiIsUser, testFiDetail.FIID, testFiDetail.FIName)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form, fiIsUser, testFiDetail.FIName)(request, messages(application)).toString
       }
     }
 
     "must redirect to the next page when valid data is submitted" in {
-
-      val mockSessionRepository = mock[SessionRepository]
-
       when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
 
       val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        applicationBuilder(userAnswers = Some(ua))
           .overrides(
-            bind[FinancialInstitutionsService].toInstance(mockFinancialInstitutionsService)
+            bind[FinancialInstitutionsService].toInstance(mockFinancialInstitutionsService),
+            bind[SessionRepository].toInstance(mockSessionRepository)
           )
           .build()
 
       running(application) {
         val request =
-          FakeRequest(POST, otherAccessRoute)
+          FakeRequest(POST, otherAccessPostRoute)
             .withFormUrlEncodedBody(("value", "true"))
-
+        println(request)
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.RemoveAreYouSureController.onPageLoad(testFiid).url
+        redirectLocation(result).value mustEqual routes.RemoveAreYouSureController.onPageLoad().url
       }
     }
     "must return a Bad Request and errors when invalid data is submitted" in {
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+      val application = applicationBuilder(userAnswers = Some(ua))
         .overrides(
-          bind[FinancialInstitutionsService].toInstance(mockFinancialInstitutionsService)
+          bind[FinancialInstitutionsService].toInstance(mockFinancialInstitutionsService),
+          bind[SessionRepository].toInstance(mockSessionRepository)
         )
         .build()
 
       running(application) {
         val request =
-          FakeRequest(POST, otherAccessRoute)
+          FakeRequest(POST, otherAccessPostRoute)
             .withFormUrlEncodedBody(("value", ""))
 
         val boundForm = form.bind(Map("value" -> ""))
@@ -115,7 +120,7 @@ class OtherAccessControllerSpec extends SpecBase with MockitoSugar with PrivateM
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, fiIsUser, testFiDetail.FIID, testFiDetail.FIName)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, fiIsUser, testFiDetail.FIName)(request, messages(application)).toString
       }
     }
 
@@ -127,10 +132,10 @@ class OtherAccessControllerSpec extends SpecBase with MockitoSugar with PrivateM
     formProvider = formProvider,
     controllerComponents = stubMessagesControllerComponents(),
     getData = mock[DataRetrievalAction],
+    requireData = mock[DataRequiredAction],
     financialInstitutionsService = mock[FinancialInstitutionsService],
     view = mock[OtherAccessView],
-    sessionRepository = mock[SessionRepository],
-    requireData = mock[DataRequiredAction]
+    sessionRepository = mock[SessionRepository]
   )(ExecutionContext.global)
 
   val getFormKey: PrivateMethod[String] = PrivateMethod[String](Symbol("getFormKey"))
