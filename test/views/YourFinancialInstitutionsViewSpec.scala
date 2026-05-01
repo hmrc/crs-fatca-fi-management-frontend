8bda692bb8e8b17e0a3269eb630fb53258e12edf
Ali Kouhestani <90757950+alikouhestani@users.noreply.github.com>
2026-05-01 11:59:05 +0100

    DAC6-4208 | passed fi name and fixed camal case for fi Id in url (#281)

--- test/views/YourFinancialInstitutionsViewSpec.scala
+++ test/views/YourFinancialInstitutionsViewSpec.scala
@@ -73,7 +73,7 @@ class YourFinancialInstitutionsViewSpec extends SpecBase with GuiceOneAppPerSuit
       getWindowTitle(doc) must include("Manage your financial institutions")
       getPageHeading(doc) mustEqual "You have added 1 financial institution"
       doc.body.select("dt").text() must include("Test Financial Institution")
-      doc.body.select(".govuk-summary-list__actions-list-item").html() must include("baseUrl?fiid=12345")
+      doc.body.select(".govuk-summary-list__actions-list-item").html() must include("baseUrl?fiId=12345")
     }
   }
 
