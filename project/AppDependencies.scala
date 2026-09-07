09a95a6ce88bbe80df5d9061e0d14c4ac3a68642
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2026-09-07 12:06:23 +0100

    DAC6-4455 | upgrade play fe version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,11 +7,11 @@ object AppDependencies {
 
   val compile = Seq[ModuleID](
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.12.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "com.beachape"      %% "enumeratum-play"                       % "1.9.0",
+    "com.beachape"      %% "enumeratum-play"                       % "1.9.8",
     "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.4.0"
   )
 
